4e0c2a8df78cc97d199b249f9c55d45ade8fad0c
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-05-26 16:36:43 -0500

    Rearrange html

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 
-const playButton = document.querySelector(".music");
+import { Header, Main } from "./components";
+import * as state from "./store";
 
-playButton.addEventListener("click", function () {
-    console.log("Play button was clicked");
-})
+
+function render(st = state.User) {
+    document.querySelector("#root").innerHTML = `
+    ${Header(st)}
+    ${Main(st)}
+    `;
+    addNavEventListeners();
+}
+
+render();
+
+// state.User
+
+
+function addNavEventListeners() {
+    document.getElementById("contact").addEventListener("click", event =>
+    {event.preventDefault();
+    render(state[event.target.textContent])});
+    document.querySelector(".user-name").addEventListener("click", event =>
+    {event.preventDefault();
+    render(state.User)});
+};
